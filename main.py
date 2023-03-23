2c5bd86c2313e9cb5c03ca8e96700fee749f9547
Sahar <77019488+SaharAbdelhaq@users.noreply.github.com>
2023-03-23 14:28:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,137 @@
+import time
+import random
+
+print("welcome to HANGMAN Game \n ")
+name = input("enter your name: ")
+print(" Hello " + name + "wish you luck! ")
+time.sleep(2)
+print("Game will started in 3 seconds.. ")
+time.sleep(3)
+
+
+
+
+def main():
+    global length
+    global word
+    global display
+    global count
+    global already_guessed
+    global play_game
+    words_to_guess = ["sahar", "love", "image", "film", "promise", "moon", "sky", "like", "heart", "damage","friend"]
+
+    word = random.choice(words_to_guess)
+
+    length= len(word)
+    display = "_" * length
+    count = 0
+    already_guessed = []
+    play_game = ""
+
+
+
+def playy():
+    global play_game
+    play_game=input("Do you want to play? y = yes,  n = no  \n ")
+    while play_game not in ["y", "n","Y","N"]:
+        play_game = input("Do You want to play again? y = yes, n = no \n")
+    if play_game == "y":
+        main()
+    elif play_game == "n":
+        print("Thanks! you should come back again!")
+        exit()
+
+
+def hangman():
+    global word
+    global count
+    global display
+    global already_guessed
+    global play_game
+    limit = 5
+
+    guess = input("This is the Hangman Word: " + display + " Enter your guess: \n")
+    guess = guess.strip()
+    if len(guess.strip()) == 0 or len(guess.strip()) >= 2 or guess <= "9":
+        print("Invalid Input, Try a letter\n")
+        hangman()
+
+    elif guess in word:
+        already_guessed.extend([guess])
+        index = word.find(guess)
+        word = word[:index] + "_" + word[index + 1:]
+        display = display[:index] + guess + display[index + 1:]
+        print(display + "\n")
+    elif guess in already_guessed:
+        print("Try another letter.\n")
+
+    else:
+     count += 1
+
+    if count == 1:
+            time.sleep(1)
+            print("   _____ \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "__|__\n")
+            print("Wrong guess. " + str(limit - count) + " guesses remaining\n")
+
+    elif count == 2:
+            time.sleep(1)
+            print("   _____ \n"
+                  "  |     | \n"
+                  "  |     |\n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "__|__\n")
+            print("Wrong guess. " + str(limit - count) + " guesses remaining\n")
+    elif count == 3:
+           time.sleep(1)
+           print("   _____ \n"
+                 "  |     | \n"
+                 "  |     |\n"
+                 "  |     | \n"
+                 "  |      \n"
+                 "  |      \n"
+                 "  |      \n"
+                 "__|__\n")
+           print("Wrong guess. " + str(limit - count) + " guesses remaining\n")
+    elif count == 4:
+            time.sleep(1)
+            print("   _____ \n"
+                  "  |     | \n"
+                  "  |     |\n"
+                  "  |     | \n"
+                  "  |     O \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "__|__\n")
+            print("Wrong guess. " + str(limit - count) + " last guess remaining\n")
+    elif count == 5:
+            time.sleep(1)
+            print("   _____ \n"
+                  "  |     | \n"
+                  "  |     |\n"
+                  "  |     | \n"
+                  "  |     O \n"
+                  "  |    /|\ \n"
+                  "  |    / \ \n"
+                  "__|__\n")
+            print("Wrong guess. You are hanged!!!\n")
+            print("The word was:",already_guessed,word)
+            playy()
+
+    if word == '_' * length:
+        print("Congrats! You have guessed the word correctly!")
+        playy()
+    elif count != limit:
+        hangman()
+
+main()
+hangman()
